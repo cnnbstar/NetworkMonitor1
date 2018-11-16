6582478467f0c5a56718213b40fef5493feda1b5
dazi <cnnbstar@163.com>
2018-11-16 14:26:26 +0800

    删除test

--- Test/WinRAT/pystatus/app/settings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-class Configuration(object):
-    DATABASE = {
-        'name': 'systemstatus.db',
-        'engine': 'peewee.SqliteDatabase',
-        'check_same_thread': False,
-    }
-    DEBUG = True,
-    SECRET_KEY = '12345678'
